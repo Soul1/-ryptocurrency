8beae5020ff35ba51b74c81e2f94f12a3ec6237e
Soul1 <darkwaud@gmail.com>
2020-04-10 01:24:12 +0300

    Updates

---
+++ precache-manifest.a333812d93e28a818d6dff39b608776f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8f541780aa8e0e0e63f9b43d96231d",
+    "url": "/Cryptocurrency/index.html"
+  },
+  {
+    "revision": "421f448e14ac57b970fe",
+    "url": "/Cryptocurrency/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "775740aec8ee65db83a1",
+    "url": "/Cryptocurrency/static/css/main.90600398.chunk.css"
+  },
+  {
+    "revision": "421f448e14ac57b970fe",
+    "url": "/Cryptocurrency/static/js/2.7c682711.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/Cryptocurrency/static/js/2.7c682711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775740aec8ee65db83a1",
+    "url": "/Cryptocurrency/static/js/main.0ad6f2be.chunk.js"
+  },
+  {
+    "revision": "b71701e271b2815c1384",
+    "url": "/Cryptocurrency/static/js/runtime-main.6ae387b4.js"
+  }
+]);
